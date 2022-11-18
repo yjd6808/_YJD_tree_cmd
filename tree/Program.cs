20bc0eb06741cea9aba375cd1c47ed1ae45aeea9
jdyun <wjdeh313@naver.com>
2022-11-18 18:22:31 +0900

    first commit

---
+++ tree/Program.cs
@@ -0,0 +1,41 @@
+﻿using CTree;
+
+string dir = Environment.CurrentDirectory;
+
+if (args.Length >= 1)
+{
+    dir = args[0];
+
+    if (!Directory.Exists(dir))
+    {
+        Console.WriteLine("올바른 디렉토리 명을 입력해주세요.");
+        Environment.Exit(-1);
+    }
+}
+
+ConsoleTree tree = new()
+{
+    ItemForegroundColor = ConsoleColor.White,
+    BridgeForegroundColor = ConsoleColor.Red
+};
+
+ConstructFileTree(tree.Root, dir);
+tree.Print();
+
+void ConstructFileTree(ConsoleTreeItem parent, string curDir)
+{
+    foreach (string subDir in Directory.GetDirectories(curDir))
+    {
+        var child = parent.AddReturnChild(new ConsoleTreeItem(Path.GetFileName(subDir)) 
+            { ForegroundColor = ConsoleColor.Cyan });
+        ConstructFileTree(child, subDir);
+    }
+
+    foreach (string file in Directory.GetFiles(curDir))
+        parent.Add(Path.GetFileName(file));
+}
+
+
+
+
+
